ac8e65a3edfd67488a432035141c569ab439444f
Eyr3 <eyr3jane@gmail.com>
2017-09-30 20:43:01 +0800

    Update C8_Detect AES in ECB mode.py

--- Crypto Challenge Set 1/C8_Detect AES in ECB mode.py
+++ Crypto Challenge Set 1/C8_Detect AES in ECB mode.py
@@ -9,10 +9,23 @@ from collections import defaultdict
 
 def repeated(buffer, block_l = 16):
     reps = defaultdict(lambda:-1)
+    #每次循环，为buffer中各block的reps值均赋值-1
+
+'''
+已知一个buffer中，存在多个字母块(block)，且单个明文字母块长度为block_l=16byte，
+利用这一串16byte的bytes流作为该block的标识，每出现一个标识，就为其reps[block]（初值为-1）赋值+1，
+当在一个buffer内block重复出现，则reps[block]值>0.
+'''
+
     for i in range(0, len(buffer), block_l):
         block = bytes(buffer[i : 1+block_l])
         reps[block] += 1
     return sum(reps.values())
+...
+sum([x,y,z])=x+y+z //x,y,z is num
+reps.values()=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  //经计算得：非AES-ECB加密buffer的value值为18个0
+ECB：reps.values()=[0, 0, 0, 0, 0, 0, 3, 0, 3, 0, 0, 0, 0, 0]  //经计算得：AES-ECB加密buffer的value值中0个数减少，并出现2个3
+...
 
 max_reps = 0
 ecb_ciphertext = ""
@@ -21,6 +34,13 @@ for ciphertext in list(open("8.txt" , "r")):
     ciphertext = ciphertext.rstrip()
     # rstrip()删除string字符串末尾的指定字符（默认为空格）,常用在读取文件中字符串时
     reps = repeated(bytearray(ciphertext))
+    
+'''
+由于16进制编码不会出现块重复的现象，即reps.value为18个0，
+而经AES-ECB加密的buffer会出现reps.value值>0的现象，
+因此，只要对reps.value中各值求和，大于0者对应buffer，即为经AES-ECB加密的密文块.
+'''
+
     if reps > max_reps:
         max_reps = reps
         ecb_ciphertext = ciphertext
